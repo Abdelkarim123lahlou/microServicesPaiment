6e6ab2335fc2f83acd1aa1ae21f341ba9af8cd65
abdelkarim123lahlou <abdelkarimlahlou.al@gmail.com>
2022-11-20 15:11:20 +0100

    centraliser la config dans depot distant

--- produitsMs/src/main/java/produims/com/ComApplication.java
+++ produitsMs/src/main/java/produims/com/ComApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ComApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.config.name", "bootstrap");// pour dire a spring de mire à partire de bootstrap au lieu de application properties
         SpringApplication.run(ComApplication.class, args);
+
     }
 
 }
